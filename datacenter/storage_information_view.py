314ca46054f109edef7ab9e62be3e804b77d80d8
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-27 01:38:06 +0300

    Get timedelta

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,6 +1,7 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils import timezone
 
 
 def storage_information_view(request):
@@ -8,7 +9,9 @@ def storage_information_view(request):
 
     all_visits = Visit.objects.all()
     non_closed_visits = all_visits.filter(leaved_at=None)
-    print(non_closed_visits)
+    for non_closed_visit in non_closed_visits:
+        print('Зашёл в хранилище, время по Москве:', timezone.localtime(non_closed_visit.entered_at), sep='\n')
+        print('Находится в хранилище:', timezone.now() - timezone.localtime(non_closed_visit.entered_at), sep='\n')
     context = {
         "non_closed_visits": non_closed_visits,  # не закрытые посещения
     }
