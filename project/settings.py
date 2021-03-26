39b26827f89fcaf37d8169e53f821b9c5bbd4409
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-27 00:00:08 +0300

    Update parametres setting.py from .env

--- project/settings.py
+++ project/settings.py
@@ -1,13 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
