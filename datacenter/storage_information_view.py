9a11b0ab09985c540985e1a7d9fc80ba29906776
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-27 12:33:53 +0300

    Get names visits is in the store

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -12,7 +12,10 @@ def storage_information_view(request):
     for non_closed_visit in non_closed_visits:
         print('Зашёл в хранилище, время по Москве:', timezone.localtime(non_closed_visit.entered_at), sep='\n')
         print('Находится в хранилище:', timezone.now() - timezone.localtime(non_closed_visit.entered_at), sep='\n')
+        print('Имя:', non_closed_visit.passcard.owner_name, sep='\n')
+    
     context = {
         "non_closed_visits": non_closed_visits,  # не закрытые посещения
     }
+
     return render(request, 'storage_information.html', context)
